e4fb1b035b98a053f0aebda6a959e3f96fde7a57
i233047 <i233047@isb.nu.edu.com>
2025-12-06 22:59:56 +0500

    fixed error in ci-cd for staging

--- saleor/settings.py
+++ saleor/settings.py
@@ -522,7 +522,12 @@ TEST_RUNNER = "saleor.tests.runner.PytestTestRunner"
 
 PLAYGROUND_ENABLED = get_bool_from_env("PLAYGROUND_ENABLED", True)
 
-ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1", "sqe-env.eba-jbsmv3h8.us-east-1.elasticbeanstalk.com"))
+ALLOWED_HOSTS = get_list(
+    os.environ.get(
+        "ALLOWED_HOSTS",
+        "localhost,127.0.0.1,sqe-env.eba-jbsmv3h8.us-east-1.elasticbeanstalk.com"
+    )
+)
 ALLOWED_GRAPHQL_ORIGINS: list[str] = get_list(
     os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*")
 )
